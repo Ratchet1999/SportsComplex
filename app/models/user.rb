6cbe17ed9b27d276925ee36d0978966ad5a117ef
Rachit Chaurasia <62827625+Ratchet1999@users.noreply.github.com>
2023-06-28 02:28:47 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   scope :current_month_enrolled_batches, ->(user){ joins(:batches).where("EXTRACT(MONTH FROM batch_time) = ? ", Time.current.strftime("%m")).where(id: user) }
-  scope :enrolled_coachings_in_branch, ->(user, branch, start_time){ joins(coachings: [:sport, :branch]).where(id: user).where(branch: { id: branch }).where("EXTRACT(MONTH FROM start_time) = ? ",start_time.strftime("%m") ) }
 end
